57dfa43efb8b93344596d031f0486d6d9309a2fe
Offichigo <oph.perianin@gmail.com>
2026-05-12 11:26:17 +0200

    requête get

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ app.get("/", async function (req, res) {
 app.listen(3000, () => {
   console.log("🚀 Serveur lancé : http://localhost:3000");
 });
+// ->Requêtes get___________________________________________________
 
 app.get("/skills/", async function (req, res) {
   const { rows } = await pool.query("SELECT * from skills");
@@ -29,12 +30,46 @@ app.get("/themes/", async function (req, res) {
   res.json(rows);
 });
 
+app.get("/themes/:id", async function (req, res) {
+  const { rows } = await pool.query("SELECT * from themes WHERE id= $1", [
+    req.params.id,
+  ]);
+  res.json(rows);
+});
+
 app.get("/resources/", async function (req, res) {
   const { rows } = await pool.query("SELECT * from resources");
   res.json(rows);
 });
 
+app.get("/resources/:id", async function (req, res) {
+  const { rows } = await pool.query("SELECT * from resources WHERE id= $1", [
+    req.params.id,
+  ]);
+  res.json(rows);
+});
+
 app.get("/resources_skills/", async function (req, res) {
   const { rows } = await pool.query("SELECT * from resources_skills");
+  res.json(rows);
+});
+
+app.get("/resources_skills/:skill_id", async function (req, res) {
+  const { skill_id } = req.params;
+
+  const { rows } = await pool.query(
+    "SELECT * FROM resources_skills WHERE skill_id = $1",
+    [skill_id],
+  );
+
+  res.json(rows);
+});
+
+app.get("/resources_skills/:skill_id/:resources_id", async function (req, res) {
+  const { skill_id, resources_id } = req.params;
+  const { rows } = await pool.query(
+    "SELECT * FROM resources_skills WHERE skill_id = $1 AND resources_id = $2",
+    [skill_id, resources_id],
+  );
   res.json(rows);
 });
