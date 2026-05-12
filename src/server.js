3a90992942bfbef7d6e4bc7dd6886ac8636af4ef
Offichigo <oph.perianin@gmail.com>
2026-05-12 20:59:02 +0200

    get post done

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import pool from "./db.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", async function (req, res) {
   const { rows } = await pool.query("SELECT * FROM resources");
@@ -11,7 +12,7 @@ app.get("/", async function (req, res) {
 app.listen(3000, () => {
   console.log("🚀 Serveur lancé : http://localhost:3000");
 });
-// ->Requêtes get___________________________________________________
+// ======= SKILLS =======
 
 app.get("/skills/", async function (req, res) {
   const { rows } = await pool.query("SELECT * from skills");
@@ -25,6 +26,26 @@ app.get("/skills/:id", async function (req, res) {
   res.json(rows);
 });
 
+app.post("/skills", async function (req, res) {
+  const { name } = req.body;
+
+  const { rows } = await pool.query(
+    "INSERT INTO skills (name) VALUES ( $1 ) RETURNING *",
+    [name],
+  );
+  res.status(201).json(rows[0]);
+});
+
+app.put("/skills/:id", async (req, res) => {
+  const { name } = req.body;
+  const { rows } = await pool.query(
+    "UPDATE skills SET name = $1 WHERE id = $2 RETURNING *",
+    [name, req.params.id],
+  );
+  res.json(rows[0]);
+});
+
+// ======= THEMES =======
 app.get("/themes/", async function (req, res) {
   const { rows } = await pool.query("SELECT * from themes");
   res.json(rows);
@@ -36,6 +57,17 @@ app.get("/themes/:id", async function (req, res) {
   ]);
   res.json(rows);
 });
+
+app.post("/themes", async (req, res) => {
+  const { name, description } = req.body;
+  const { rows } = await pool.query(
+    "INSERT INTO themes (name, description) VALUES ($1, $2) RETURNING *",
+    [name, description],
+  );
+  res.status(201).json(rows[0]);
+});
+
+// ======= RESOURCES =======
 
 app.get("/resources/", async function (req, res) {
   const { rows } = await pool.query("SELECT * from resources");
@@ -49,12 +81,22 @@ app.get("/resources/:id", async function (req, res) {
   res.json(rows);
 });
 
+app.post("/resources", async (req, res) => {
+  const { type, title, description, url, is_ada, theme_id } = req.body;
+  const { rows } = await pool.query(
+    "INSERT INTO resources (type, title, description, url, is_ada, theme_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    [type, title, description, url, is_ada, theme_id],
+  );
+  res.status(201).json(rows[0]);
+});
+// ======= RESOURCES_SKILLS =======
+
 app.get("/resources_skills/", async function (req, res) {
   const { rows } = await pool.query("SELECT * from resources_skills");
   res.json(rows);
 });
 
-app.get("/resources_skills/:skill_id", async function (req, res) {
+app.get("/resources_skills/skill/:skill_id", async function (req, res) {
   const { skill_id } = req.params;
 
   const { rows } = await pool.query(
@@ -65,11 +107,20 @@ app.get("/resources_skills/:skill_id", async function (req, res) {
   res.json(rows);
 });
 
-app.get("/resources_skills/:skill_id/:resources_id", async function (req, res) {
-  const { skill_id, resources_id } = req.params;
+app.get("/resources_skills/resources/:resources_id", async function (req, res) {
+  const { resources_id } = req.params;
   const { rows } = await pool.query(
-    "SELECT * FROM resources_skills WHERE skill_id = $1 AND resources_id = $2",
-    [skill_id, resources_id],
+    "SELECT * FROM resources_skills WHERE resources_id = $1",
+    [resources_id],
   );
   res.json(rows);
+});
+
+app.post("/resources_skills", async (req, res) => {
+  const { resources_id, skill_id } = req.body;
+  const { rows } = await pool.query(
+    "INSERT INTO resources_skills (resources_id, skill_id) VALUES ($1, $2) RETURNING *",
+    [resources_id, skill_id],
+  );
+  res.status(201).json(rows[0]);
 });
